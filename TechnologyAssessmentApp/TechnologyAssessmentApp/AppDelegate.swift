eee58a4033c79bf83c5821dca592f6e810d5e461
Kshitij Godara <kshitij.godara@nagarro.com>
2018-08-10 15:41:11 +0530

    Trailing Space Removed

--- TechnologyAssessmentApp/TechnologyAssessmentApp/AppDelegate.swift
+++ TechnologyAssessmentApp/TechnologyAssessmentApp/AppDelegate.swift
@@ -9,10 +9,7 @@
 import UIKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
-    //is Git Updated
-    
+class AppDelegate: UIResponder, UIApplicationDelegate {    
     var window: UIWindow?
     func application(_ application: UIApplication, didFinishLaunchingWithOptions
         launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
